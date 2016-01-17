5aa972b998590701e8945ce82c4b9638ef3b2a84
Gianluca Supino <gianluca.supino@gmail.com>
2016-01-17 20:14:32 +0100

    working on adapterMomgo

--- dev/adapterMongo.js
+++ dev/adapterMongo.js
@@ -2,49 +2,140 @@ const MongoClient = require('mongodb').MongoClient
 const _ = require('lodash');
 const ObjectId = require('mongodb').ObjectId;
 
-export default class AdapterMongo{
+/*
+find
+findOneAndDelete
+findOneAndReplace
+findOneAndUpdate
 
-    constructor(db){
-        this._db=db;
+options:{
+    sort: name field whit - sign for desc
+    skip: number
+    limit: number:
+    fields: {name:1}
+
+}
+
+*/
+export default class AdapterMongo {
+
+    constructor(db) {
+        this._db = db;
     }
 
-    static async connect(url,options){
+    static async connect(url, options) {
         if (typeof(options) === 'undefined') {
-            options = { };
+            options = {};
         }
-        try{
-            return await MongoClient.connect(url,options);
-        }catch(err){
+        try {
+            return await MongoClient.connect(url, options);
+        } catch (err) {
             throw err;
         }
 
     }
 
-    async save(collection,id,values){
-        
+    async save(collection, values, id) {
+        try {
+            const col = this._db.collection(collection);
+            // TODO: I'd like to just use update with upsert:true, but I'm
+            // note sure how the query will work if id == null. Seemed to
+            // have some problems before with passing null ids.
+            if (_.isUndefined(id)) {
+                let result = await col.insertOne(values);
+                if (!result.hasOwnProperty('insertedId') || result.insertedId === null) {
+                    throw (new Error('Save failed to generate ID for object.'));
+                }
+                return result;
+            } else {
+                id=castId(id);
+                return await col.updateOne({
+                    _id: id
+                }, {
+                    $set: values
+                });
+            }
+        } catch (err) {
+            throw err;
+        }
+
     }
 
-    async delete(collection,id){
-        
-        
+    async delete(collection, id) {
+
+
     }
 
-    
 
 
 
 
 
+    async loadOne(collection, query, projection) {
+        query = castQueryIds(query);
+        try {
+            const col = this._db.collection(collection);
+            let cursor = await col.find(query, projection).limit(1);
+            return cursor.next()
+        } catch (err) {
+            throw err;
+        }
 
-    async count(collection, query){
-        query=castQueryIds(query);
-        try{
-            const col=this._db.collection(collection);
+    }
+
+
+
+    async loadMany(collection, query, options = {}) {
+        query = castQueryIds(query);
+        try {
+            const col = this._db.collection(collection);
+            let cursor = col.find(query);
+            cursor = this._optionsToCursor(cursor, options);
+            return await cursor.toArray();
+        } catch (err) {
+            throw err;
+        }
+
+    }
+
+
+    async count(collection, query) {
+        query = castQueryIds(query);
+        try {
+            const col = this._db.collection(collection);
             return await col.count(query);
-        }catch(err){
+        } catch (err) {
             throw err;
         }
     }
+
+    //TODO : permit more fields i sort
+
+    _optionsToCursor(cursor, options) {
+        if (typeof options.sort === 'string') {
+            let sortOrder = 1;
+            if (options.sort[0] === '-') {
+                sortOrder = -1;
+                options.sort = options.sort.substring(1);
+            }
+            let sortOptions = {};
+            sortOptions[options.sort] = sortOrder;
+            cursor = cursor.sort(sortOptions);
+        }
+        if (typeof options.skip === 'number') {
+            cursor = cursor.skip(options.skip);
+        }
+        if (typeof options.limit === 'number') {
+            cursor = cursor.limit(options.limit);
+        }
+        if (_.isObject(options.fields)) {
+            cursor = cursor.project(options.fields);
+        }
+
+        return cursor;
+    }
+
+
 
 }
 
--- dev/work.js
+++ dev/work.js
@@ -6,10 +6,25 @@ import  AdapterMongo from './adapterMongo';
 async function main(){
     let db=await AdapterMongo.connect('mongodb://localhost:27017/mariabakery-dev');
     let adapterMongo= new AdapterMongo(db);
-    let res=await adapterMongo.count('ingredients');
 
-
+    /*
+        TEST FIND
+    */
+    /*
+    let startDate=new Date;
+    let res=await adapterMongo.loadMany('ingredients',{foodGroup:'0800'},{limit:1000,fields:{foodGroup:1}});
+    console.log(new Date-startDate);
     console.log(res)
+    */
+    /*
+        TEST SAVE
+    */
+    let resSave=await adapterMongo.save('test',{a:1,titolo:'questo è un test'});
+    //let resSave=await adapterMongo.save('test',{a:3},'569be52f8d0237f5173ca2b2');
+    console.log(resSave)
+
+
+    //console.log(res)
     db.close();
 }
 
